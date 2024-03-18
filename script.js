c00713370b69e47d6d640d03eb2df0c9e4f27121
shatarupa mohanty <shatarupa.mohanty1002@gmail.com>
2024-03-18 14:37:20 +0530

    another api soption

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let qrdiv = document.querySelector(".qrbox");
 let qrimg = document.getElementById("qrImg");
 function generateQR() {
     if (userip.value.length > 0) {
+        // `https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=${userip}`; another api try both
         qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + userip.value;
         setInterval(() => {
             qrdiv.classList.add("show");
